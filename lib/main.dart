c227f8258bc2c6b68a61bfbbed0e5c607e266382
Varun Sharma <imva508@gmail.com>
2019-01-15 23:13:19 +0530

    Added a Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My App",
-      home: Scaffold(
-        appBar: AppBar(title:Text("Home")),
-        body: Center(
-          child: Text("I love flutter"),
-        ),
+        debugShowCheckedModeBanner: false, title: "My App", home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String text = "Hello world";
+
+  void _onChanged(){
+    setState(() {
+      if(text.startsWith("H"))
+        text= "Welcome to flutter";
+      else
+        text = "Hello world";
+    });
+  }
+
+  Widget _bodyWidget() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Center(
+        child: Column(children: <Widget>[
+          Text(text),
+          RaisedButton(child: Text("Click me"),
+          onPressed: _onChanged,)
+        ],),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      body: _bodyWidget(),
     );
   }
 }
