327d43a8462de2adffc643d16bdbcaf4eeffd46c
Varun Sharma <imva508@gmail.com>
2019-01-15 22:57:27 +0530

    disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "My App",
       home: Scaffold(
         appBar: AppBar(title:Text("Home")),
