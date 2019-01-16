99b5a527abc0ce244575a634144b4b0b85ac8a0d
Varun Sharma <imva508@gmail.com>
2019-01-16 06:01:04 +0530

    Added custom assets and scrim to images

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: LoginPage(), theme: ThemeData(primarySwatch: Colors.blue));
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => LoginPageState();
+}
+
+class LoginPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image(
+            image: AssetImage("assets/image.jpg",),
+            fit: BoxFit.fill,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          )
+        ],
+      ),
+    );
+  }
+}
